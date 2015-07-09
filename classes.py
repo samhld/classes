7f201bfadb1e2f8bccd3700d5fcd13d25de3fe86
Sam Dillard <samhdillard@gmail.com>
2015-07-09 00:38:45 +0000

    fifth

--- classes.py
+++ classes.py
@@ -7,41 +7,45 @@ class Musician(object):
             print self.sounds[i % len(self.sounds)]
         print ""
  
-class Bassist(Musician):
-    def __init__(self):
-        #call the __init__ method of the parent class
-        print "my name is David and I'm the guitarist"
-        super(Bassist, self).__init__(["Twang", "Thrumb", "Bling"])
+    class Bassist(Musician):
+        def __init__(self):
+            #call the __init__ method of the parent class
+            print "my name is David and I'm the guitarist"
+            super(Bassist, self).__init__(["Twang", "Thrumb", "Bling"])
         
-class Guitarist(Musician):
-    def __init__(self):
-        super(Guitarist, self).__init__(["Boink", "Bow", "Boom"])
+    class Guitarist(Musician):
+        def __init__(self):
+            super(Guitarist, self).__init__(["Boink", "Bow", "Boom"])
     
-    def tune(self):
-        print "Be with you in a moment"
-        print "Twoning, sproing, splang"
+        def tune(self):
+            print "Be with you in a moment"
+            print "Twoning, sproing, splang"
  
-class Drummer(Musician):
-    def __init__(self):
-        super(Drummer, self).__init__(["Boom","Boom","Clap"])
+    class Drummer(Musician):
+        def __init__(self):
+            super(Drummer, self).__init__(["Boom","Boom","Clap"])
         
-    def fourCount(self):
-        for i in range(1,5):
-            print i
-            time.sleep(.5)
-    def combust(self):
+        def fourCount(self):
+            for i in range(1,5):
+                print i
+                time.sleep(.5)
+        def combust(self):
         
 class Band(object):
     def __init__(self):
-        pass
+    
+    bandMembers = [
     
     def hire(bandMember):
+        bandMembers.append(bandMember)
         
+    def fire(bandMember):
+        bandMembers.remove(bandMember)
         
 
-david = Bassist()
-derek = Guitarist()
-david.solo(6)
+    david = Bassist()
+    derek = Guitarist()
+    david.solo(6)
 
-print david.solo
+    print david.sounds
 
